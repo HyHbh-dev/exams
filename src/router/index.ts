c11c38cf9352d17505000a1f7f36d9190efd9fba
hyhbh <54164460+HyHbh-dev@users.noreply.github.com>
2024-09-07 20:52:26 +0800

    修改为历史模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 const routes = [
   {
     path: '/mbti',
@@ -20,6 +20,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(), // 使用 createWebHistory
+  history: createWebHashHistory(), // 使用 createWebHistory
   routes
 })
